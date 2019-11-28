aca651f8e6661b7c9a3b4dc308979c7a9e7831cd
moconn22 <moconn22@nd.edu>
2019-11-28 13:33:38 -0500

    more progress

--- Exercise09.R
+++ Exercise09.R
@@ -3,19 +3,18 @@ library(readxl)
 data_ <- read_excel("data???.xlsx")
 View(data_)                                                                   
 library(ggplot2)
-c<-ggplot(data = data_, aes(x=data_$`Birth Year`, y=data_$`Average Height`))
-c+geom_point()+theme_bw()+xlab("Birth Year")+ylab("Average Height")+geom_line()
+c<-ggplot(data = data_, aes(x=BirthYear, y=AverageHeight))
+c+geom_point()+theme_bw()+xlab("Birth Year")+ylab("Average Height")+geom_smooth(method="lm")
 
 #2
+#bar plot of means
 library(ggplot2)
 moredata <- read.csv("~/Desktop/Sophomore_Year/bc/examples/exercises/ICB2019_Exercise09/data.txt")
-d<-ggplot(data = moredata, aes(x=moredata$region, y=moredata$observations))
+d<-ggplot(data = moredata, aes(x=region, y=observations))
+d+geom_bar( stat="summary", fun.y="mean")
 
-newmatrix<-matrix(nrow = 4, ncol = 2)
 
-for (observations in 1:nrow(moredata)){
-  mean()
-}
+
 
 
 #scatter plot of observations
@@ -26,10 +25,10 @@ d+geom_point()+geom_jitter()
   
   
   
+#to have just one chart is misleading because with the averages, it appears as though the distributions are around the same numbers. When you look at the scatterplot too, it is apparent that just one misleading, look at distribution 
   
   
   
   
-  
-)
+
 
