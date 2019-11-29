0b6424f5031b705f28b7f7b085c64a18d07520f1
moconn22 <moconn22@nd.edu>
2019-11-28 19:23:24 -0500

    almost final

--- Exercise09.R
+++ Exercise09.R
@@ -13,19 +13,10 @@ moredata <- read.csv("~/Desktop/Sophomore_Year/bc/examples/exercises/ICB2019_Exe
 d<-ggplot(data = moredata, aes(x=region, y=observations))
 d+geom_bar( stat="summary", fun.y="mean")
 
-
-
-
-
 #scatter plot of observations
-d+geom_point()+geom_jitter()
-  
-  
-  
-  
-  
-  
-#to have just one chart is misleading because with the averages, it appears as though the distributions are around the same numbers. When you look at the scatterplot too, it is apparent that just one misleading, look at distribution 
+d+geom_jitter()
+
+#The bar plots and the scatter plots do tell different stories because if you were to just look at the bar plot, the means would indicate that all of the observations were around a very similar number, 15. However, when looking at the scatterplot, it becomes clear that some of the regions have a wider distribution and are a lot more spread out and some have a smaller distribution and are more centered around a certain number, showing that the data sets are actually a lot more different than the bar-plot of the means suggests.  Therefore, it is necessary to have both plots for a more complete picture of the data set. 
   
   
   
